e0560999201dbcb77de8873d35da7c1ed3ab7096
matsmy <101180563+matsmy@users.noreply.github.com>
2022-06-14 09:27:45 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,42 @@
+(function () {
+  const example = document.getElementById('example')
+  const cw1 = document.getElementById('cw1')
+  const cw2 = document.getElementById('cw2')
+  const cw3 = document.getElementById('cw3')
+  const answer = document.getElementById('answer')
+
+  example.addEventListener("click", function () {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(array => {
+        console.log(array)
+        answer.innerHTML = JSON.stringify(array);
+      })
+  })
+
+  cw1.addEventListener("click", function () {
+    answer.innerHTML="Ładowanie...";
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response=>response.json())
+        .then(function (dane){
+          NowaLinia(dane)
+        })
+    function NowaLinia(dane){
+      answer.innerHTML="";
+      for (var i =0; i <dane.length;i++){
+        var div = document.createElement("div");
+        div.innerHTML= '<mark><strong>UserID: ' + dane[i].userId + '</strong></mark><br><em>id: ' + dane[i].id +'</br></em>' + ' title: ' + dane[i].title + '<br> body: '+ dane[i].body + '<br><hr>';
+        answer.appendChild(div);
+      }
+    }
+  })
+
+  cw2.addEventListener("click", function () {
+     //TODO implement it
+  })
+
+  cw3.addEventListener("click", function () {
+    //TODO implement it
+  })
+
+})();
